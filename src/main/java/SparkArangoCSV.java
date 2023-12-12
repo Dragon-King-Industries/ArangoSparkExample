49be4e0c0e2dee21755bb0b7f78b70da2239ac9c
th37rose <thomasr37oss@gmail.com>
2023-12-12 15:41:42 -0500

    Bugs fixing2

--- src/main/java/SparkArangoCSV.java
+++ src/main/java/SparkArangoCSV.java
@@ -1,21 +1,24 @@
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDBException;
+import com.arangodb.entity.BaseDocument;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
-import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
-import org.apache.spark.sql.types.StructField;
-import java.util.HashMap;
-import scala.reflect.ClassTag$;
 
-import java.util.Map;
-import java.util.Properties;
+import java.io.File;
+import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 
 public class SparkArangoCSV {
     public static void main(String[] args) throws TimeoutException, StreamingQueryException {
+        final String ARANGO_HOST = "localhost";
+        final int ARANGO_PORT = 8529;
+        final String ARANGO_USER = "root";
+        final String ARANGO_PASS = "123456";
+        final String ARANGO_DB_NAME = "testdb";
+        final String ARANGO_COLLECTION_NAME = "test";
+
         // Initialize Spark
         SparkSession spark = SparkSession.builder()
                 .appName("csv_to_arangodb")
@@ -23,38 +26,92 @@ public class SparkArangoCSV {
                 .getOrCreate();
         //local[*]
 
-        // // Define the path to your large CSV file
-        // String inputFile = "../SparkDataIngestion/input_file.csv"; // Replace with your file path
-
         // The path to the directory to monitor
-        String inputDir = "../SparkDataIngestion"; // Replace with your directory path
+        String inputDir = "./SparkDataIngestion";
+
+        // Create the directory if it does not exist
+        File directory = new File(inputDir);
+        if (!directory.exists()) {
+            boolean mkdirs = directory.mkdirs();
+            if (!mkdirs) {
+                System.err.println("Failed to create directory: " + inputDir);
+                return;
+            }
+        }
+
+        // Check if the directory contains any CSV files
+        File[] files = directory.listFiles((dir, name) -> name.toLowerCase().endsWith(".csv"));
+        if (files == null || files.length == 0) {
+            System.err.println("No CSV files found in directory: " + inputDir);
+            return;
+        }
         
         // Infer schema from existing data
         StructType csvSchema = spark.read()
                 .format("csv")
                 .option("header", "true")
                 .option("inferSchema", "true")
-                .load(inputDir)
+                .load(inputDir + "/*.csv")
                 .schema();
 
         Dataset<Row> df = spark.readStream()
                 .format("csv")
                 .schema(csvSchema)
+                .option("inferSchema", "true")
                 .option("header", "true")
                 .load(inputDir);
 
-        Map<String, String> arangoOpts = new HashMap<String, String>();
-        arangoOpts.put("password", "rootpassword");
-        arangoOpts.put("table", "test");
-        arangoOpts.put("ssl.enabled", "False");
-        arangoOpts.put("endpoints", "localhost:8529");
+        // Custom ForeachWriter to send the data to ArangoDB
+        ForeachWriter<Row> writer = new ForeachWriter<Row>() {
+            private transient ArangoDB arangoDB;
+
+            @Override
+            public boolean open(long partitionId, long epochId) {
+                try {
+                    arangoDB = new ArangoDB.Builder()
+                            .host(ARANGO_HOST, ARANGO_PORT)
+                            .user(ARANGO_USER)
+                            .password(ARANGO_PASS)
+                            .build();
+                    return true;
+                } catch (Exception e) {
+                    System.err.println("Failed to establish ArangoDB connection: " + e.getMessage());
+                    return false;
+                }
+            }
+
+            @Override
+            public void process(Row row) {
+                BaseDocument doc = new BaseDocument();
+                Arrays.stream(row.schema().fields()).forEach(field -> {
+                    String fieldName = field.name();
+                    Object fieldValue = row.getAs(fieldName);
+                    doc.addAttribute(fieldName, fieldValue);
+                });
+
+                try {
+                    arangoDB.db(ARANGO_DB_NAME).collection(ARANGO_COLLECTION_NAME).insertDocument(doc);
+                } catch (ArangoDBException e) {
+                    System.err.println("Failed to insert document: " + e.getMessage());
+                }
+            }
+
+            @Override
+            public void close(Throwable errorOrNull) {
+                if (arangoDB != null) {
+                    try {
+                        arangoDB.shutdown();
+                    } catch (Exception e) {
+                        System.err.println("Failed to close ArangoDB connection: " + e.getMessage());
+                    }
+                }
+            }
+        };
 
         // Write data to ArangoDB as it's read
         StreamingQuery query = df.writeStream()
-                .format("com.arangodb.spark")
-                .mode(SaveMode.Append)
-                .options(arangoOpts)
-                .option("checkpointLocation", "checkpoints/")
+                .foreach(writer)
+                .outputMode("append")
                 .start();
 
         query.awaitTermination();
--- src/main/java/SparkArangoJSON.java
+++ src/main/java/SparkArangoJSON.java
@@ -1,16 +1,24 @@
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SaveMode;
-import org.apache.spark.sql.SparkSession;
+import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDBException;
+import com.arangodb.entity.BaseDocument;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.types.StructType;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 
 public class SparkArangoJSON {
     public static void main(String[] args) throws TimeoutException, StreamingQueryException {
+        final String ARANGO_HOST = "localhost";
+        final int ARANGO_PORT = 8529;
+        final String ARANGO_USER = "root";
+        final String ARANGO_PASS = "123456";
+        final String ARANGO_DB_NAME = "testdb";
+        final String ARANGO_COLLECTION_NAME = "test";
+
         // Initialize Spark
         SparkSession spark = SparkSession.builder()
                 .appName("json_to_arangodb")
@@ -18,29 +26,89 @@ public class SparkArangoJSON {
                 .getOrCreate();
         //local[*]
 
-        // // Define the path to your large CSV file
-        // String inputFile = "../SparkDataIngestion/input_json.json"; // Replace with your file path
-
         // The path to the directory to monitor
-        String inputDir = "../SparkDataIngestion"; // Replace with your directory path
+        String inputDir = "./SparkDataIngestion";
+
+        // Create the directory if it does not exist
+        File directory = new File(inputDir);
+        if (!directory.exists()) {
+            boolean mkdirs = directory.mkdirs();
+            if (!mkdirs) {
+                System.err.println("Failed to create directory: " + inputDir);
+                return;
+            }
+        }
+
+        // Check if the directory contains any JSON files
+        File[] files = directory.listFiles((dir, name) -> name.toLowerCase().endsWith(".json"));
+        if (files == null || files.length == 0) {
+            System.err.println("No JSON files found in directory: " + inputDir);
+            return;
+        }
+
+        // Infer schema from existing data
+        StructType jsonSchema = spark.read()
+                .format("json")
+                .load(inputDir)
+                .schema();
         
         Dataset<Row> df = spark.readStream()
                 .format("json")
+                .schema(jsonSchema)
                 .option("multiline", "true") 
-                .load(inputDir);
+                .load(inputDir + "/*.json");
 
-        Map<String, String> arangoOpts = new HashMap<String, String>();
-        arangoOpts.put("password", "rootpassword");
-        arangoOpts.put("table", "test2");
-        arangoOpts.put("ssl.enabled", "False");
-        arangoOpts.put("endpoints", "localhost:8529");
+        // Custom ForeachWriter to send the data to ArangoDB
+        ForeachWriter<Row> writer = new ForeachWriter<Row>() {
+            private transient ArangoDB arangoDB;
+
+            @Override
+            public boolean open(long partitionId, long epochId) {
+                try {
+                    arangoDB = new ArangoDB.Builder()
+                            .host(ARANGO_HOST, ARANGO_PORT)
+                            .user(ARANGO_USER)
+                            .password(ARANGO_PASS)
+                            .build();
+                    return true;
+                } catch (Exception e) {
+                    System.err.println("Failed to establish ArangoDB connection: " + e.getMessage());
+                    return false;
+                }
+            }
+
+            @Override
+            public void process(Row row) {
+                BaseDocument doc = new BaseDocument();
+                Arrays.stream(row.schema().fields()).forEach(field -> {
+                    String fieldName = field.name();
+                    Object fieldValue = row.getAs(fieldName);
+                    doc.addAttribute(fieldName, fieldValue);
+                });
+
+                try {
+                    arangoDB.db(ARANGO_DB_NAME).collection(ARANGO_COLLECTION_NAME).insertDocument(doc);
+                } catch (ArangoDBException e) {
+                    System.err.println("Failed to insert document: " + e.getMessage());
+                }
+            }
+
+            @Override
+            public void close(Throwable errorOrNull) {
+                if (arangoDB != null) {
+                    try {
+                        arangoDB.shutdown();
+                    } catch (Exception e) {
+                        System.err.println("Failed to close ArangoDB connection: " + e.getMessage());
+                    }
+                }
+            }
+        };
 
         // Write data to ArangoDB as it's read
-        StreamingQuery query = df.write()
-                .format("com.arangodb.spark")
-                .mode(SaveMode.Append)
-                .options(arangoOpts)
-                .option("checkpointLocation", "checkpoints/")
+        StreamingQuery query = df.writeStream()
+                .foreach(writer)
+                .outputMode("append")
                 .start();
 
         query.awaitTermination();
